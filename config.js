2371623da1c452856ba48fff2115b419160ed3e8
Nikhil4474 <ninuganti15@gmail.com>
2022-08-17 17:04:01 -0700

    first commit

---
+++ config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+  apiKey: "AIzaSyAyQDOTzY_XEFhs7jIFcT-z6P5HcS1TZS8",
+  authDomain: "e-ride-app-c2771.firebaseapp.com",
+  projectId: "e-ride-app-c2771",
+  storageBucket: "e-ride-app-c2771.appspot.com",
+  messagingSenderId: "413400296479",
+  appId: "1:413400296479:web:b9bd2f967fff82ebe9792a"
+};
+
+
+
+
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
+
+
